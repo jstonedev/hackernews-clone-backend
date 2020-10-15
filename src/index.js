c7f30afc3d68cf5a0619aed630121b18e8283d1a
jstonedev <jstonedev521@gmail.com>
2020-10-14 23:47:15 -0400

    update: extend API with full CRUD functionality for the Link type

--- src/index.js
+++ src/index.js
@@ -9,11 +9,44 @@ let links = [
 	},
 ];
 
+let idCount = links.length;
+
 // Resolvers
 const resolvers = {
 	Query: {
 		info: () => `This is the API of a Hackernews Clone`,
 		feed: () => links,
+		link: (root, { id }) => {
+			const link = links.find((link) => link.id === id);
+			return link;
+		},
+	},
+	Mutation: {
+		post: (root, args) => {
+			const link = {
+				id: `link-${idCount++}`,
+				description: args.description,
+				url: args.url,
+			};
+			links.push(link);
+			return link;
+		},
+		updateLink: (root, args) => {
+			links.forEach((link) => {
+				if (link.id === args.id) {
+					link.id = args.id;
+					link.url = args.url;
+					link.description = args.description;
+				}
+				return link;
+			});
+		},
+		deleteLink: (root, { id }) => {
+			const index = links.findIndex((link) => link.id === id);
+			const link = links[index];
+			links.splice(index, 1);
+			return link;
+		},
 	},
 };
 
